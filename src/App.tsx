7e874b3e99b8e7f8b0f37e018d96d8f3ce5cae05
websivustot <websivustot@gmail.com>
2023-12-05 11:57:19 +0200

    d3 react snowpack chart initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,34 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
+import Bars from './Bars';
 import './App.css';
 
 interface AppProps {}
 
 function App({}: AppProps) {
+  const dimensions = {
+    height: 600,
+    width: 400,
+  };
+  const [data, setData] = useState<number[]>([]);
   // Create the count state.
   const [count, setCount] = useState(0);
   // Create the counter (+1 every second).
   useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000);
+    const timer = setTimeout(() => setCount(count + 1), 100);
+
+    if (data.length < 10) {
+      data.push(Math.random());
+    } else {
+      const newData = data.splice(1, 10);
+      setData(newData);
+    }
     return () => clearTimeout(timer);
   }, [count, setCount]);
   // Return the App component.
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
+        <Bars data={data} dimensions={dimensions} />
       </header>
     </div>
   );
